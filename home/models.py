9b769193afe68f8f74f42380720d70f87e53fa9d
Benjamin Bach <benjamin@overtag.dk>
2021-11-11 00:06:25 +0100

    Initial empty Wagtail project, linted with pre-commit, black, flake8

---
+++ home/models.py
@@ -0,0 +1,6 @@
+from django.db import models  # noqa
+from wagtail.core.models import Page
+
+
+class HomePage(Page):
+    pass
---
+++ migcontrol/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*wj40^s9l!%9lqdjld7$)ue_k71_4c3f9)#dsaadh()=r=6l8!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
