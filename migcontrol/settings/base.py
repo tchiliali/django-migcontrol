470dc812b6efcb561bfccc822459bd23ff394eb5
Benjamin Bach <benjamin@overtag.dk>
2021-11-13 23:48:25 +0100

    Add error logging

--- migcontrol/settings/base.py
+++ migcontrol/settings/base.py
@@ -91,6 +91,32 @@ DATABASES = {
     }
 }
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "log_errors": {"level": "DEBUG", "class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins", "log_errors"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "django.db": {"handlers": ["log_errors"], "level": "ERROR", "propagate": True},
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
