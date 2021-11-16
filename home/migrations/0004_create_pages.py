bd2c7cb8bcb41f3d5ad1bf24e2dde83a0b74075d
Benjamin Bach <benjamin@overtag.dk>
2021-11-17 00:28:58 +0100

    WIP

---
+++ home/migrations/0004_create_pages.py
@@ -0,0 +1,61 @@
+from django.db import migrations
+
+# slug: title
+pages = {
+
+}
+
+
+def create_pages(apps, schema_editor):
+    # Get models
+    Site = apps.get_model('wagtailcore.Site')
+    Article = apps.get_model('home.Article')
+    ContentType = apps.get_model('contenttypes.ContentType')
+    Locale = apps.get_model('wagtailcore.Locale')
+    from wagtail.core.models import Page, Locale as LocaleNonMigrated  # noqa
+
+    # Create content type for blogindexpage model
+    homepage_content_type, __ = ContentType.objects.get_or_create(
+        model='homepage', app_label='home')
+
+    home = Page.objects.get(pk=Site.objects.get(is_default_site=True).root_page.pk)
+    locale = Locale.objects.get(pk=LocaleNonMigrated.get_default().pk)
+
+    for index, (slug, title) in enumerate(pages.items()):
+        index_offset = index + 1
+        # Create a new homepage
+        article = Article(
+            title=title,
+            draft_title=title,
+            slug=slug,
+            content_type=homepage_content_type,
+            locale=locale,
+            path=f"00010001000{index_offset}",
+            depth=3,
+            numchild=0,
+            url_path=f"/home/{slug}/",
+            live=True,
+        )
+
+        home.add_child(instance=article)
+
+
+def remove_pages(apps, schema_editor):
+    # Get models
+    BlogIndexPage = apps.get_model('blog.BlogIndexPage')
+
+    # Delete BlogIndexPage
+    # Page and Site objects CASCADE
+    BlogIndexPage.objects.filter(slug="blog").delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0002_create_homepage'),
+        ('wagtailcore', '0053_locale_model'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_pages, create_pages),
+    ]
--- home/models.py
+++ home/models.py
@@ -3,4 +3,23 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
+    """
+    This is the landing page
+    """
+
     pass
+
+
+class Article(Page):
+    """
+    We are using this model as a default article page. This covers the following
+    page types:
+
+    * Landing page
+    * About page
+    * Contact page
+    * Donate page
+    * Subscribe page
+    * Data protection page
+    * Imprint page
+    """
