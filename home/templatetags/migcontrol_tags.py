a85909ab221acffdbf4367199e6bd3fd566f5aec
Benjamin Bach <benjamin@overtag.dk>
2022-05-23 00:06:24 +0200

    Localize the root page before listing menu items

--- home/templatetags/migcontrol_tags.py
+++ home/templatetags/migcontrol_tags.py
@@ -61,3 +61,11 @@ def get_static_thumbnail(file_: str, geometry, *args, **kwargs):
             *args,
             **kwargs,
         )
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    # This returns a core.Page. The main menu needs to have the site.root_page
+    # defined else will return an object attribute error ('str' object has no
+    # attribute 'get_children')
+    return Site.find_for_request(context["request"]).root_page
