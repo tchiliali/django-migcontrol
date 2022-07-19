ec9941099ce7fb930b5a87c1db2f97078fde620d
Benjamin Bach <benjamin@overtag.dk>
2022-07-19 23:52:48 +0200

    WIP but failing - slugs and locale need to be unique together

--- blog/management/commands/wordpress_to_wagtail.py
+++ blog/management/commands/wordpress_to_wagtail.py
@@ -786,48 +786,34 @@ class Command(BaseCommand):
         origin_url,
         **kwargs,
     ):
+        mappings = self.mappings(
+            index,
+            locale,
+            post_id,
+            published,
+            title,
+            date,
+            slug,
+            body,
+            excerpt,
+            user,
+            authors,
+            meta,
+            origin_url,
+        )
+
+        if mappings.get("locale") and mappings.get("locale") != locale:
+            locale = mappings.get("locale")
+            index = index._meta.model.objects.get(
+                slug=index.slug, locale=mappings.get("locale")
+            )
+            print("Changed to index: {} {}".format(index, index.locale))
 
         try:
-            new_entry = self.PostModel.objects.get(slug=slug)
-            for k, v in self.mappings(
-                index,
-                locale,
-                post_id,
-                published,
-                title,
-                date,
-                slug,
-                body,
-                excerpt,
-                user,
-                authors,
-                meta,
-                origin_url,
-            ).items():
+            new_entry = self.PostModel.objects.get(slug=slug, locale=locale)
+            for k, v in mappings.items():
                 setattr(new_entry, k, v)
         except self.PostModel.DoesNotExist:
-
-            mappings = self.mappings(
-                index,
-                locale,
-                post_id,
-                published,
-                title,
-                date,
-                slug,
-                body,
-                excerpt,
-                user,
-                authors,
-                meta,
-                origin_url,
-            )
-
-            if mappings.get("locale") != locale:
-                index = index._meta.model.objects.get(
-                    slug=index.slug, locale=mappings.get("locale")
-                )
-                print("Changed to index: {} {}".format(index, index.locale))
 
             try:
                 new_entry = index.add_child(
